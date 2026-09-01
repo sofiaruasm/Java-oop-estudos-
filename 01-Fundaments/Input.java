bf3f107e14e02ae0be5afd9a62c56ebf1a442b2c
Sofia Ruas <sofiarmmarques@gmail.com>
2026-09-01 18:02:17 -0300

    feat: adiciona o input

---
+++ 01-Fundaments/Input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Input {
+
+    public static void main(String[] args){
+        Scanner nome = new Scanner(System.in);
+
+        String x;
+        x = nome.next();
+        System.out.println(x);
+
+
+        nome.close();
+    }
+    
+}
